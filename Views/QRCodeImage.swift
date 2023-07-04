23074c396e3ed1f510d7db6a8672954773660b4d
Mathijs Bernson <mathijsb@q42.nl>
2023-07-04 16:04:57 +0200

    Fix QRCodeImage so that it uses 100% SwiftUI

--- Views/QRCodeImage.swift
+++ Views/QRCodeImage.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct QRCodeImage: View {
     let data: Data?
@@ -23,38 +24,23 @@ struct QRCodeImage: View {
     }
 
     var body: some View {
-        ImageView(uiImage: generateQRCode(from: data))
-            .aspectRatio(contentMode: .fit)
-
-        // Image(uiImage: generateQRCode(from: data) ?? UIImage())
-        //     .resizable()
-        //     .scaledToFit()
+        Image(uiImage: generateQRCode(from: data) ?? UIImage())
+            .resizable()
+            .interpolation(.none)
+            .scaledToFit()
     }
 
     private func generateQRCode(from data: Data?) -> UIImage? {
         guard let data else { return nil }
-        guard let qrFilter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
-        qrFilter.setValue(data, forKey: "inputMessage")
+        let filter = CIFilter.qrCodeGenerator()
+        filter.message = data
 
-        let scale: CGFloat = 10
-        let transform = CGAffineTransform(scaleX: scale, y: scale)
-        let qrImage = qrFilter.outputImage?.transformed(by: transform)
+        let context = CIContext()
 
-        let uiImage = qrImage.flatMap(UIImage.init)
-        return uiImage?.withRenderingMode(.alwaysOriginal)
+        return filter.outputImage
+            .flatMap { context.createCGImage($0, from: $0.extent) }
+            .map { UIImage(cgImage: $0) }
     }
-}
-
-private struct ImageView: UIViewRepresentable {
-    let uiImage: UIImage?
-
-    func makeUIView(context: Context) -> UIImageView {
-        let imageView = UIImageView(image: uiImage)
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }
-
-    func updateUIView(_ uiView: UIImageView, context: Context) {}
 }
 
 struct QRCodeImage_Previews: PreviewProvider {
