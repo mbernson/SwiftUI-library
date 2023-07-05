d93595225c858e86ae05bb8fffd7df484e601765
Mathijs Bernson <mathijsb@q42.nl>
2023-07-05 09:46:01 +0200

    Simplify ImageViewer, make it center align the image

--- Views/ImageViewer.swift
+++ Views/ImageViewer.swift
@@ -10,69 +10,54 @@ import UIKit
 
 struct ImageViewer: UIViewControllerRepresentable {
     let image: UIImage?
-    let maximumZoomScale: CGFloat = 3.0
-    let showsVerticalScrollIndicator: Bool = false
-    let showsHorizontalScrollIndicator: Bool = false
+    let maximumZoomScale: CGFloat
+    let showsIndicators: Bool
+
+    init(image: UIImage?, maximumZoomScale: CGFloat = 3.0, showsIndicators: Bool = true) {
+        self.image = image
+        self.maximumZoomScale = maximumZoomScale
+        self.showsIndicators = showsIndicators
+    }
 
     func makeUIViewController(context: Context) -> ImageViewerController {
-        ImageViewerController(image: image)
+        return ImageViewerController()
     }
 
     func updateUIViewController(_ viewController: ImageViewerController, context: Context) {
         viewController.setImage(image)
         viewController.scrollView.maximumZoomScale = maximumZoomScale
-        viewController.scrollView.showsVerticalScrollIndicator = showsVerticalScrollIndicator
-        viewController.scrollView.showsHorizontalScrollIndicator = showsHorizontalScrollIndicator
+        viewController.scrollView.showsVerticalScrollIndicator = showsIndicators
+        viewController.scrollView.showsHorizontalScrollIndicator = showsIndicators
     }
 }
 
 class ImageViewerController: UIViewController, UIScrollViewDelegate {
-    private let imageView: UIImageView
-    private let containerView: UIView
-    let scrollView: UIScrollView
-
-    init(image: UIImage?) {
-        imageView = UIImageView(image: image)
-        scrollView = UIScrollView()
-        containerView = UIView()
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private let imageView = UIImageView()
+    fileprivate let scrollView = UIScrollView()
 
     func setImage(_ image: UIImage?) {
         imageView.image = image
+        resetZoom(animated: false)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Setup views
-        scrollView.delegate = self
         view.addSubview(scrollView)
-        scrollView.addSubview(containerView)
-        containerView.addSubview(imageView)
+        scrollView.addSubview(imageView)
 
-        // Setup constraints
+        imageView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        imageView.contentMode = .scaleAspectFit
+
+        scrollView.delegate = self
+        scrollView.autoresizesSubviews = true
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        containerView.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.topAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-
-            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-
-            imageView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
-            imageView.heightAnchor.constraint(equalTo: containerView.heightAnchor),
         ])
 
         // Add double tap to zoom gesture
@@ -81,37 +66,25 @@ class ImageViewerController: UIViewController, UIScrollViewDelegate {
         scrollView.addGestureRecognizer(doubleTapRecognizer)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        resetZoom(animated: false)
+    }
+
     @objc private func didDoubleTap(_ sender: UITapGestureRecognizer) {
         if scrollView.zoomScale == scrollView.minimumZoomScale {
-            let zoomScale: CGFloat = 2.0
-            let point = sender.location(in: containerView)
-            let zoomRect = CGSize(
-                width: scrollView.contentSize.width / zoomScale,
-                height: scrollView.contentSize.height / zoomScale
-            )
-            scrollView.zoom(to: CGRect(origin: point, size: zoomRect), animated: true)
+          scrollView.setZoomScale(scrollView.maximumZoomScale / 2, animated: true)
         } else {
-            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+          scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
         }
     }
 
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-
-        updateMinZoomScaleForSize(scrollView.frame.size)
-    }
-
-    private func updateMinZoomScaleForSize(_ size: CGSize) {
-        let widthScale = size.width / imageView.bounds.width
-        let heightScale = size.height / imageView.bounds.height
-        let minScale = min(widthScale, heightScale)
-
-        scrollView.minimumZoomScale = minScale
-        scrollView.zoomScale = minScale
+    func resetZoom(animated: Bool) {
+      scrollView.setZoomScale(scrollView.minimumZoomScale, animated: animated)
     }
 
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return containerView
+        return imageView
     }
 }
 
