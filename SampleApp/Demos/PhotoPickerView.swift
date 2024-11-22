da39771e82f84e19197ccdcca97c997370885cc4
Mathijs Bernson <mathijsb@q42.nl>
2024-11-22 11:05:26 +0100

    Update PhotoPicker preview

--- SampleApp/Demos/PhotoPickerView.swift
+++ SampleApp/Demos/PhotoPickerView.swift
@@ -6,21 +6,40 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct PhotoPickerView: View {
-    @State var presentPhotoPicker = false
+    @State var isPresentingPhotoPicker = false
+    @State var selectedImage: UIImage?
 
     var body: some View {
-        Button("Present photo picker") {
-            presentPhotoPicker.toggle()
+        VStack {
+            Button("Present photo picker") {
+                isPresentingPhotoPicker.toggle()
+            }
+
+            if let selectedImage {
+                Image(uiImage: selectedImage)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
         }
-        .sheet(isPresented: $presentPhotoPicker) {
-            PhotoPicker { items in
-                // Do something with the resulting NSItemProviders here
-                print(items)
-                presentPhotoPicker = false
+        .sheet(isPresented: $isPresentingPhotoPicker) {
+            PhotoPicker(filter: .images, selectionLimit: 1, preferredAssetRepresentationMode: .compatible) { providers in
+                isPresentingPhotoPicker = false
+                guard providers.count == 1, let provider = providers.first else { return }
+                _ = provider.loadDataRepresentation(forTypeIdentifier: UTType.image.identifier) { data, error in
+                    if let data, let image = UIImage(data: data) {
+                        DispatchQueue.main.async {
+                            selectedImage = image
+                        }
+                    } else if let error {
+                        print(error)
+                    }
+                }
             } dismiss: {
-                presentPhotoPicker = false
+                isPresentingPhotoPicker = false
             }
         }
     }
